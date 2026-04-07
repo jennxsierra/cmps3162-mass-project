9aff2f77e1e2a4121696e08d8bde350933bfc58b
Jennessa Sierra <2021153908@ub.edu.bz>
2026-04-06 19:33:35 -0600

    feat(db): created migration for the users table

---
+++ migrations/000012_create_users_table.down.sql
@@ -0,0 +1,4 @@
+-- migrations/000012_create_users_table.down.sql
+-- Removes users table.
+
+DROP TABLE IF EXISTS users;
---
+++ migrations/000012_create_users_table.up.sql
@@ -0,0 +1,12 @@
+-- migrations/000012_create_users_table.up.sql
+-- Adds users table for authentication/authorization support.
+
+CREATE TABLE IF NOT EXISTS users (
+    id bigserial PRIMARY KEY,
+    created_at timestamp(0) WITH TIME ZONE NOT NULL DEFAULT NOW(),
+    username text NOT NULL,
+    email citext UNIQUE NOT NULL,
+    password_hash bytea NOT NULL,
+    activated bool NOT NULL,
+    version integer NOT NULL DEFAULT 1
+);
