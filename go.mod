31552fe4710a8544b7ab8bc1245b9f92b6434b8e
Jennessa Sierra <2021153908@ub.edu.bz>
2026-04-06 19:34:32 -0600

    feat(api): implement users model with bcrypt password support

--- go.mod
+++ go.mod
@@ -6,4 +6,7 @@ require github.com/julienschmidt/httprouter v1.3.0
 
 require github.com/lib/pq v1.11.2
 
-require golang.org/x/time v0.15.0
+require (
+	golang.org/x/crypto v0.49.0
+	golang.org/x/time v0.15.0
+)
---
+++ internal/data/users.go
@@ -0,0 +1,53 @@
+package data
+
+import (
+	"errors"
+	"time"
+
+	"golang.org/x/crypto/bcrypt"
+)
+
+type User struct {
+	ID        int64     `json:"id"`
+	CreatedAt time.Time `json:"created_at"`
+	Username  string    `json:"username"`
+	Email     string    `json:"email"`
+	Password  password  `json:"-"`
+	Activated bool      `json:"activated"`
+	Version   int       `json:"-"`
+}
+
+// define the password type: plaintext and hash
+// lowercase to keep it from being exported and made public
+type password struct {
+	plaintext *string
+	hash      []byte
+}
+
+// Set method to hash the plaintext password and store it in the password struct
+func (p *password) Set(plaintextPassword string) error {
+	hash, err := bcrypt.GenerateFromPassword([]byte(plaintextPassword), 12)
+	if err != nil {
+		return err
+	}
+
+	p.plaintext = &plaintextPassword
+	p.hash = hash
+
+	return nil
+}
+
+// Matches method to compare the plaintext password with the hashed password
+func (p *password) Matches(plaintextPassword string) (bool, error) {
+	err := bcrypt.CompareHashAndPassword(p.hash, []byte(plaintextPassword))
+	if err != nil {
+		switch {
+		case errors.Is(err, bcrypt.ErrMismatchedHashAndPassword):
+			return false, nil
+		default:
+			return false, err
+		}
+	}
+
+	return true, nil // password matches
+}
